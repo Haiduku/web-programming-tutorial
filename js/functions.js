ba6beb243bdb4f89376d861efd3526898a0cd4c6
Haiduku <mihai.traian.daniel@gmail.com>
2015-03-27 19:48:44 +0200

    started design with playing cards

--- js/functions.js
+++ js/functions.js
@@ -1,42 +1,39 @@
 // show/hide more info
 document.getElementById('show-info').onclick = function () {
-    document.getElementById('more-info').style.display='inline';
-    document.getElementById('show-info').style.display='none';
+    document.getElementById('more-info').style.display = 'inline';
+    document.getElementById('show-info').style.display = 'none';
 };
 
 document.getElementById('hide-info').onclick = function () {
-    document.getElementById('more-info').style.display='none';
-    document.getElementById('show-info').style.display='inline';
+    document.getElementById('more-info').style.display = 'none';
+    document.getElementById('show-info').style.display = 'inline';
 };
 
-// collapse/expand news widget
-var newsWidget = document.getElementById("news-widget");
-newsWidget.getElementsByClassName("header")[0].onclick = function () {
-    if (newsWidget.className == "widget") {
-        newsWidget.className = "widget collapsed";
-    } else {
-        newsWidget.className = "widget";
-    }
-};
+function initAllWidgets() {
 
-var linksHelp = document.getElementById("help-widget");
-linksHelp.getElementsByClassName("header")[0].onclick = function() {
-    if (linksHelp.className == "widget") {
-        linksHelp.className = "widget collapsed";
-    } else {
-        linksHelp.className = "widget";
+    function initWidget(widget) {
+        widget.getElementsByClassName("header")[0].onclick = function () {
+            if (widget.className == "widget") {
+                widget.className = "widget collapsed";
+            } else {
+                widget.className = "widget";
+            }
+        }
     }
-};
-var google = document.getElementById("google-search");
-google.getElementsByClassName("header")[0].onclick = function() {
-    if (google.className == "widget") {
-        google.className = "widget collapsed";
-    } else {
-        google.className = "widget";
-    }
-};
 
-// actions for top links
+    var widgets = document.getElementsByClassName('widget');
+
+
+    for (var i = 0; i < widgets.length; i++) {
+
+        initWidget(widgets[i]);
+
+    }
+}
+initAllWidgets();
+
+
+//actions for top links
 var links = document.getElementById("top-menu").getElementsByTagName("a");
 for (var i = 0; i < links.length; i++) {
     links[i].onclick = function () {
@@ -45,21 +42,29 @@ for (var i = 0; i < links.length; i++) {
 }
 
 var days = document.getElementById("calendar-widget").getElementsByTagName("td");
-var today = new Date().getDate() ;
-for (var i=0; i<days.length; i++){
-    if(days[i].innerHTML == today){
+var today = new Date().getDate();
+for (var i = 0; i < days.length; i++) {
+    if (days[i].innerHTML == today) {
         days[i].className = "today";
     }
 }
 
 var selectedTds = document.getElementById("calendar-widget").getElementsByTagName("td");
-for (var i=0; i<selectedTds.length; i++){
-   selectedTds[i].onclick=function(){
-      //selectedTds[i].className = "today";
-      this.className = "today";
-   }
+for (var i = 0; i < selectedTds.length; i++) {
+    selectedTds[i].onclick = function () {
+        //selectedTds[i].className = "today";
+        this.className = "today";
+    }
 
 }
 
-
-
+var parametri = {
+    name: 'eu',
+    phone: '0727898463'
+    //married: 'false',
+    //skills: ['none', 'zero']
+};
+$.ajax({
+    url: 'index.html',
+    data: parametri
+});
